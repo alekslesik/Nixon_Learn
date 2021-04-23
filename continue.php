75934b0d7e2113f431c5f5f83b95f8af47e10d0b
alekslesik <alekslesik@gmail.com>
2021-04-23 11:53:10 +0300

    Add section destroy

--- continue.php
+++ continue.php
@@ -6,9 +6,17 @@ if(isset($_SESSION['username'])) {
     $forename = $_SESSION['forename'];
     $surname = $_SESSION['surname'];
 
+    destroy_session_and_data();
+
     echo "Welcome $forename. <br>
      Your whole name $forename $surname.<br>
      Your login '$username'
      Your password '$password'" ;
 }
 else echo "Please for entry <a href='auth2.php'>click here'</a>.";
+
+function destroy_session_and_data() {
+    $_SESSION = array();
+    setcookie(session_name(), '', time() - 2592000, '/');
+    session_destroy();
+}
